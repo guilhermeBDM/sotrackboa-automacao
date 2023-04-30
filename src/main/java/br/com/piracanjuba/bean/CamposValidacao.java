97f0e16bdfdbd82f6078a831ac5acbb278bb5690
GuilhermePC <guilhermeborba79@gmail.com>
2023-04-30 19:02:24 -0300

    src/java

---
+++ src/main/java/br/com/piracanjuba/bean/CamposValidacao.java
@@ -0,0 +1,20 @@
+package br.com.piracanjuba.bean;
+
+import java.util.ArrayList;
+
+
+public class CamposValidacao
+{
+	private String placa;
+	private String carga;
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	
+}
